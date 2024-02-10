96ee7ec2c06bf8fab9f9a060e974230239a37494
Pablo Alan <pabloxt14@gmail.com>
2024-02-10 18:28:50 -0300

    feat: config cookie to identify each user on vote

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,15 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 
 const app = fastify()
+
+app.register(cookie, {
+  secret: 'nlw-expert-poll', // for cookies signature
+  hook: 'onRequest', // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
+})
 
 app.register(createPoll)
 app.register(getPoll)
