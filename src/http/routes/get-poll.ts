d83935ac8c6cdb6001bcfd0499d7dc4cd5f501aa
Pablo Alan <pabloxt14@gmail.com>
2024-02-11 18:13:22 -0300

    feat: using redis on getPoll route

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import z from 'zod'
 import { prisma } from '../../lib/prisma'
+import { redis } from '../../lib/redis'
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -25,9 +26,33 @@ export async function getPoll(app: FastifyInstance) {
     })
 
     if (!poll) {
-      return reply.status(404).send({ message: 'Pesquisa não encontrada' })
+      return reply.status(404).send({ message: 'Enquete não encontrada' })
     }
 
-    return reply.send({ poll })
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = result.reduce(
+      (accumulator, current, index) => {
+        if (index % 2 === 0) {
+          const score = Number(result[index + 1])
+          accumulator[current] = score
+        }
+        return accumulator
+      },
+      {} as Record<string, number>,
+    )
+
+    const pollWithVotes = {
+      id: poll.id,
+      title: poll.title,
+      options: poll.options.map((option) => {
+        return {
+          ...option,
+          score: votes[option.id] ?? 0,
+        }
+      }),
+    }
+
+    return reply.send({ poll: pollWithVotes })
   })
 }
